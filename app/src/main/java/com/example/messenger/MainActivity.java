daac9c18666d75f08916e698c78254b6b657e321
alphamustang <ryan.noel@hotmail.com>
2019-06-25 13:51:00 +0100

    Added a text box to collect input, and added android:onClick to the button so that when it is clicked, a little message is displayed in the textbox on the bottom of the screen.

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view) {
+        TextView displayedText = (TextView) findViewById(R.id.textView1);
+        displayedText.setText("This is just testing everything is working.");
     }
 }
